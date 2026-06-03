736a1aad26be3f7bf50f85fae1f0ddade3be7e89
NikolaevAntonFE <NikolaevAntonFE@users.noreply.github.com>
2026-06-03 17:48:43 +0300

    Add smart quick task input

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "mobile-task-tracker-v39";
+const cacheName = "mobile-task-tracker-v40";
 const assets = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./capacitor.js"];
 
 self.addEventListener("install", event => {

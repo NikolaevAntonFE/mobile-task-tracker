0b991fc9949c8ca45731b9ea0071a574b2ae5677
NikolaevAntonFE <NikolaevAntonFE@users.noreply.github.com>
2026-06-03 17:34:09 +0300

    Add focused task filters

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "mobile-task-tracker-v38";
+const cacheName = "mobile-task-tracker-v39";
 const assets = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./capacitor.js"];
 
 self.addEventListener("install", event => {

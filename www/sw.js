7925f3dd5f37813accd858314bde553597ef6ffa
NikolaevAntonFE <NikolaevAntonFE@users.noreply.github.com>
2026-06-04 11:30:23 +0300

    Close choice sheets via backdrop

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "mobile-task-tracker-v48";
+const cacheName = "mobile-task-tracker-v49";
 const assets = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./capacitor.js"];
 
 self.addEventListener("install", event => {

4c71ccd0b28881b5a4a01f7ec083b84b8f4ab8cb
NikolaevAntonFE <NikolaevAntonFE@users.noreply.github.com>
2026-06-04 11:46:42 +0300

    Show editable no due pill

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "mobile-task-tracker-v49";
+const cacheName = "mobile-task-tracker-v50";
 const assets = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./capacitor.js"];
 
 self.addEventListener("install", event => {

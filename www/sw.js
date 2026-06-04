e9ca7447f2650c2ac010e0f121357d24cc666101
NikolaevAntonFE <NikolaevAntonFE@users.noreply.github.com>
2026-06-04 11:17:58 +0300

    Add inline task attribute editing

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "mobile-task-tracker-v46";
+const cacheName = "mobile-task-tracker-v47";
 const assets = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./capacitor.js"];
 
 self.addEventListener("install", event => {

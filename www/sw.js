7e3d8c0361c10f625daa00a37fee86e3c4d0b4a1
NikolaevAntonFE <NikolaevAntonFE@users.noreply.github.com>
2026-06-04 10:40:36 +0300

    Rename inbox tab to timeless

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,4 @@
-const cacheName = "mobile-task-tracker-v41";
+const cacheName = "mobile-task-tracker-v42";
 const assets = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg", "./capacitor.js"];
 
 self.addEventListener("install", event => {
